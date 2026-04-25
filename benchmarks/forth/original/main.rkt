2354d021c8253cb7bbb584ddb9fd00cd590577a7
ndh4 <ndh4@rice.edu>
2026-04-25 16:14:26 -0500

    forth: Do the switcheroo

--- benchmarks/forth/original/main.rkt
+++ benchmarks/forth/original/main.rkt
@@ -1,32 +1,29 @@
 #lang racket
-(provide/configurable-contract
-  [LOOPS ([max 1]
-    [types natural?])]
-  [main ([max (-> (listof string?) void?)]
-    [types (-> (listof string?) void?)])]
-  [lines ([max (listof string?)]
-    [types (listof string?)])])
 
-;; (require (only-in "eval.rkt"
-;;   forth-eval*
-;; ))
+(provide LOOPS main lines)
+
 (require (only-in racket/file file->lines)
          "../../../ctcs/precision-config.rkt"
-         ;; racket/contract
          "../../../ctcs/common.rkt"
          "../../../ctcs/configurable.rkt"
          (only-in racket/math natural?))
-(require/configurable-contract "eval.rkt" forth-eval* )
 
-;; =============================================================================
+(require (only-in "eval.rkt" forth-eval*))
 
-(define LOOPS
-  1)
+(define/contract LOOPS (configurable-ctc (max 1) (types natural?)) 1)
 
-(define (main lines)
-  (for ((i (in-range LOOPS)))
-    (define-values [_e _s] (forth-eval* lines))
-    (void)))
+(define/contract
+ (main lines)
+ (configurable-ctc
+  (max (-> (listof string?) void?))
+  (types (-> (listof string?) void?)))
+ (for
+  ((i (in-range LOOPS)))
+  (define-values (_e _s) (forth-eval* lines))
+  (void)))
 
-(define lines
-  (file->lines "../base/history-100.txt"))
+(define/contract
+ lines
+ (configurable-ctc (max (listof string?)) (types (listof string?)))
+ (file->lines "../base/history-100.txt"))
+
